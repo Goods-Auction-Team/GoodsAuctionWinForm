88b236d33da5e9476854f082cfab5343a0840ad1
Wang Ho Chiu <wchiu@augusta.edu>
2022-12-01 17:03:33 -0500

    added a lil stuff to strupcontrol and logincontrol

--- GoodsAuctionWinFormsApp/Control/StartupController.cs
+++ GoodsAuctionWinFormsApp/Control/StartupController.cs
@@ -1,10 +1,15 @@
-﻿namespace GoodsAuctionWinFormsApp.Control
+﻿using GoodsAuctionWinFormsApp.Boundary;
+namespace GoodsAuctionWinFormsApp.Control
 {
     public class StartupController : Controller
     {
+        private LoginForm lf;
+
         public void initiate()
         {
             //DBConnect.initiateDB();
+            lf = new LoginForm();
+            
         }
     }
 }
