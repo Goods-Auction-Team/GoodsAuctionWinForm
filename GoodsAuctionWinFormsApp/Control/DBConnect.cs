a4761c8871b111c59a16c8500d2b83aa96e72d79
jadshouse <98120629+jadshouse@users.noreply.github.com>
2022-12-06 16:22:30 -0500

    crap

--- GoodsAuctionWinFormsApp/Control/DBConnect.cs
+++ GoodsAuctionWinFormsApp/Control/DBConnect.cs
@@ -1,12 +1,4 @@
-﻿using GoodsAuctionWinFormsApp.Boundary;
-using GoodsAuctionWinFormsApp.Entity;
-using System.Collections.Generic;
-using System.ComponentModel.Design;
-using System.Data;
-using System.Data.SQLite;
-using System;
-using Microsoft.Data.Sqlite;
-using System.Collections;
+﻿using GoodsAuctionWinFormsApp.Entity;
 
 namespace GoodsAuctionWinFormsApp.Control
 {
@@ -22,8 +14,6 @@ namespace GoodsAuctionWinFormsApp.Control
         {
             this.username = username;
         }
-
-        
 
         public Account getUser()
         {
@@ -64,36 +54,36 @@ namespace GoodsAuctionWinFormsApp.Control
             }
             else { return false; }
         }
-        private static string connStr = @"Data Source=Auctiondb.db"; //@"Data Source =.;Initial Catalog = Auctiondb; Trusted_Connection=true";
+        private static string conn = @"Data Source =.;Initial Catalog = Auctiondb; Trusted_Connection=true";
 
         public static void InitDB()
         {
 
-            using (SQLiteConnection conn = new SQLiteConnection(connStr))
-            {
-                using (SQLiteCommand cmnd = new SQLiteCommand())
+            using (SQLiteConnection conn = new SQLiteConnection(@"Data Source=Auctiondb.mdf;"))
 
-                {
-                    conn.Open();
-                    cmnd.Connection = conn;
-                    string strSql = @"BEGIN TRANSACTION; 
+            using (SQLiteCommand cmnd = new SQLiteCommand())
+
+            {
+                conn.Open();
+                cmnd.Connection = conn;
+                string strSql = @"BEGIN TRANSACTION; 
                      DROP TABLE IF EXISTS ACCOUNT;
                      DROP TABLE IF EXISTS ITEM;
                      DROP TABLE IF EXISTS LOGOUT;
                      DROP TABLE IF EXISTS BIDS;
                      COMMIT;";
 
-                    cmnd.CommandText = strSql;
-                    cmnd.ExecuteNonQuery();
-                    string table = @"CREATE TABLE [ACCOUNT] (ID int NOT NULL IDENTIFY(1,1),
+                cmnd.CommandText = strSql;
+                cmnd.ExecuteNonQuery();
+                string table = @"CREATE TABLE [ACCOUNT] (
                                  [username] TEXT PRIMARY KEY NOT NULL
                                , [password] INTEGER NOT NULL
                                , [type] TEXT NOT NULL
-                               , CONSTRAINT [PK_ACCOUNT] PRIMARY KEY ([ID]));";
+                               , CONSTRAINT [PK_ACCOUNT] PRIMARY KEY ([username]));";
 
-                    cmnd.CommandText = table;
-                    cmnd.ExecuteNonQuery();
-                    table = @"CREATE TABLE [ITEM] (
+                cmnd.CommandText = table;
+                cmnd.ExecuteNonQuery();
+                table = @"CREATE TABLE [ITEM] (
                     [ItemId] INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL
                     , [ItemName] TEXT NOT NULL
                     , [ItemDescription] TEXT NOT NULL
@@ -105,9 +95,9 @@ namespace GoodsAuctionWinFormsApp.Control
                     , CONSTRAINT [PK_ITEM] PRIMARY KEY ([ItemId])
                     , FOREIGN KEY([Seller]) REFERENCES [ACCOUNT]([username]));";
 
-                    cmnd.CommandText = table;
-                    cmnd.ExecuteNonQuery();
-                    table = @"CREATE TABLE [LOGOUT] (
+                cmnd.CommandText = table;
+                cmnd.ExecuteNonQuery();
+                table = @"CREATE TABLE [LOGOUT] (
                                 [Id] TEXT NOT NULL
                               , [Date] INTEGER NOT NULL
                               , [Time] INTEGER NOT NULL
@@ -115,19 +105,15 @@ namespace GoodsAuctionWinFormsApp.Control
                               , CONSTRAINT [PK_LOGOUT] PRIMARY KEY ([Id])
                               , FOREIGN KEY([UName]) REFERENCES [ACCOUNT]([username]));";
 
-                    cmnd.CommandText = table;
-                    cmnd.ExecuteNonQuery();
-                    table = @"CREATE TABLE [BIDS] (
+                cmnd.CommandText = table;
+                cmnd.ExecuteNonQuery();
+                table = @"CREATE TABLE [BIDS] (
                                 [ItemNo] TEXT PRIMARY KEY NOT NULL
                               , [BuyerNo] TEXT PRIMARY KEY NOT NULL
                               , [Amount] INTEGER NOT NULL
                               , CONSTRAINT [PK_BIDS] PRIMARY KEY ([ItemNo],[BuyerNo]));";
 
-                    ReadData(conn);
-
-                }
             }
-
         }
 
 
@@ -135,7 +121,7 @@ namespace GoodsAuctionWinFormsApp.Control
         {
             SQLiteCommand sqliteCommand;
             sqliteCommand = conn.CreateCommand();
-            sqliteCommand.CommandText = "INSERT INTO ACCOUNT(username" +
+            sqliteCommand.CommandText = "INSERT INTO ACCOUT(username" +
                 "                                          , password" +
                 "                                          , type) VALUES ('','','')";
             sqliteCommand.ExecuteNonQuery();
@@ -173,126 +159,38 @@ namespace GoodsAuctionWinFormsApp.Control
             sqliteCommand.ExecuteNonQuery();
         }
 
-        public static void ReadData()
+        public static void ReadData(SQLiteConnection conn)
         {
-            //SQLiteDataReader sqliteReader;
-            //SQLiteCommand sqliteCommand;
-            //sqliteCommand = conn.CreateCommand();
-
-
-//sqliteCommand.CommandText = "SELECT * FROM ITEM";
-//sqliteReader = sqliteCommand.ExecuteReader();
-//while (sqliteReader.Read())
-//{
-//    string readerstring = sqliteReader.GetString(0);
-//}
-/*
-        table = @"CREATE TABLE [ITEM] (
-        [ItemId] INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL
-        , [ItemName] TEXT NOT NULL
-        , [ItemDescription] TEXT NOT NULL
-        , [StatingBid] INTEGER NOT NULL                     
-        , [currentBid] INTEGER NOT NULL
-        , [timeRemaining] INTEGER NOT NULL
-        , [currentLeader] TEXT NOT NULL
-        , [Seller] TEXT NOT NULL
-        , CONSTRAINT [PK_ITEM] PRIMARY KEY ([ItemId])
-        , FOREIGN KEY([Seller]) REFERENCES [ACCOUNT]([username]));";*/
-using (SQLiteConnection conn = new SQLiteConnection(connStr))
-{
-    string sql = "SELECT * FROM ITEM;";
-    using (SQLiteCommand cmd = new SQLiteCommand(sql, conn))
-
-    {
-        using (SQLiteDataReader r = cmd.ExecuteReader())
-        {
-            string str = "";
-            while (r.Read())
+            SQLiteDataReader sqliteReader;
+            SQLiteCommand sqliteCommand;
+            sqliteCommand = conn.CreateCommand();
+            sqliteCommand.CommandText = "SELECT * FROM ACCOUNT";
+            sqliteReader = sqliteCommand.ExecuteReader();
+            while (sqliteReader.Read())
             {
-                r.GetString(0);
+                string readerstring = sqliteReader.GetString(0);
+            }
+
+            sqliteCommand.CommandText = "SELECT * FROM ITEM";
+            sqliteReader = sqliteCommand.ExecuteReader();
+            while (sqliteReader.Read())
+            {
+                string readerstring = sqliteReader.GetString(0);
+            }
+
+            sqliteCommand.CommandText = "SELECT * FROM LOGOUT";
+            sqliteReader = sqliteCommand.ExecuteReader();
+            while (sqliteReader.Read())
+            {
+                string readerstring = sqliteReader.GetString(0);
+            }
+
+            sqliteCommand.CommandText = "SELECT * FROM BIDS";
+            sqliteReader = sqliteCommand.ExecuteReader();
+            while (sqliteReader.Read())
+            {
+                string readerstring = sqliteReader.GetString(0);
             }
         }
     }
-}
-
-
-        Item item1 = new Item();
-Item item2 = new Item();
-item1.setItemDescription("Mega Pie!");
-item1.setItemID(1);
-item1.setItemName("MosterPie");
-item1.setTimeRemaining((DateTime.Now).AddMinutes(1));
-
-item2.setItemName("Vrrrrm Car");
-item2.setItemID(2);
-item2.setItemDescription("Car go fast!");
-item2.setTimeRemaining((DateTime.Now).AddMinutes(5));
-
-StartupController.iList.Add(item1);
-StartupController.iList.Add(item2);
-
-
-
-
-}
-
-
-public static Account findAccount(string username)
-{
-//SQLiteCommand sqliteCommand;
-//sqliteCommand = conn.CreateCommand();
-//sqliteCommand.CommandText = "SELECT * FROM ACCOUNT";
-//sqliteReader = sqliteCommand.ExecuteReader();
-//while (sqliteReader.Read())
-//{
-//    Account import = new Account();
-//    import.SetUsername(sqliteReader.GetString(0));
-//    import.SetPassword(int.Parse(sqliteReader.GetString(1)));
-//    import.SetType(bool.Parse(sqliteReader.GetString(2)));
-
-//}
-
-bool found = false;
-
- Account andrewAccount = new Account();
-andrewAccount.SetPassword("password".GetHashCode());
-andrewAccount.SetUsername("aroe@augusta.edu");
-andrewAccount.SetType(false);
-
-Account Joy = new Account();
-Joy.SetPassword("password".GetHashCode());
-Joy.SetUsername("Joy@shutup.net");
-Joy.SetType(true);
-
-
-Account user = new Account();
-
-if (andrewAccount.getUsername() == username)
-    return andrewAccount;
-if (Joy.getUsername() == username)
-     return Joy;
-else
-    return null;
-}
-
-public static void UpdateItemDatabase(ItemList list)
-{
-
-//overwrites ITEMs with this list
-
-}
-
-public static void UpdateLastLogout(Account loggingOutAccount)
-{
-//saves Account.getUsername & DATETIME
-}
-
-public static void UpdateBids(Item item, Account account)
-{
-//Saves Item Name, bid, and account's username
-}
-
-
-
-}
 }
