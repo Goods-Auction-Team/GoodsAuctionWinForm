804cd3b892b67340e2c5199a94dd44711b3fb593
yalch01 <yalch01@yahoo.com>
2022-12-05 23:12:54 -0500

    newer DB

--- GoodsAuctionWinFormsApp/Control/DBConnect.cs
+++ GoodsAuctionWinFormsApp/Control/DBConnect.cs
@@ -1,40 +1,94 @@
 ﻿using GoodsAuctionWinFormsApp.Boundary;
 using GoodsAuctionWinFormsApp.Entity;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Data.SQLite;
+using System;
 
 namespace GoodsAuctionWinFormsApp.Control
 {
-    public static class DBConnect
+    public class DBConnect : Controller
+
     {
+        private Account user;
+        private Item itemID;
+        private ItemList item;
+        private Account username;
+
+        public DBConnect(Account username)
+        {
+            this.username = username;
+        }
+
+        public Account getUser()
+        {
+            return username;
+        }
+        public void saveLogout(Account username)
+        {
+            this.username = username;
+        }
+
+        public Item getItem(Item itemID)
+        {
+            //This item ID returns the item
+            return null;
+        }
+
+        public ItemList modifyItem(Item itemID, string newBid, Account username)
+        {
+            return null;//This is the placebid where it updates the item after bid and 
+                        //returns ItemList
+            return null;
+        }
+
+        public ItemList saveItem(Item item)
+        {
+            //This is the addItem where the data is saved
+            return null;
+        }
+
+        // adding saveLogout(un) from sequence diagram
+        public bool saveLogout(string username)
+        {
+            // not sure if this is right, but I'm trying to check if it saved correctly
+            // if true, sends it back to LoginForm for (logged out sucess)
+            if (username != null)
+            {
+                return true;
+            }
+            else { return false; }
+        }
+        private static string conn = @"Data Source =.;Initial Catalog = Auctiondb; Trusted_Connection=true";
 
         public static void InitDB()
         {
 
-            using (SQLiteConnection conn = new SQLiteConnection(@"data source = nAuctionDb.db"))
+            using (SQLiteConnection conn = new SQLiteConnection(@"Data Source=Auctiondb.mdf;"))
+
+            using (SQLiteCommand cmnd = new SQLiteCommand())
+
             {
-                using (SQLiteCommand cmnd = new SQLiteCommand())
-                {
-                    conn.Open();
-                    cmnd.Connection = conn;
-                    string strSql = @"BEGIN TRANSACTION; 
+                conn.Open();
+                cmnd.Connection = conn;
+                string strSql = @"BEGIN TRANSACTION; 
                      DROP TABLE IF EXISTS ACCOUNT;
                      DROP TABLE IF EXISTS ITEM;
                      DROP TABLE IF EXISTS LOGOUT;
                      DROP TABLE IF EXISTS BIDS;
                      COMMIT;";
 
-                    cmnd.CommandText = strSql;
-                    cmnd.ExecuteNonQuery();
-                    string table = @"CREATE TABLE [ACCOUNT] (
+                cmnd.CommandText = strSql;
+                cmnd.ExecuteNonQuery();
+                string table = @"CREATE TABLE [ACCOUNT] (
                                  [username] TEXT PRIMARY KEY NOT NULL
                                , [password] INTEGER NOT NULL
                                , [type] TEXT NOT NULL
                                , CONSTRAINT [PK_ACCOUNT] PRIMARY KEY ([username]));";
 
-                    cmnd.CommandText = table;
-                    cmnd.ExecuteNonQuery();
-                    table = @"CREATE TABLE [ITEM] (
+                cmnd.CommandText = table;
+                cmnd.ExecuteNonQuery();
+                table = @"CREATE TABLE [ITEM] (
                     [ItemId] INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL
                     , [ItemName] TEXT NOT NULL
                     , [ItemDescription] TEXT NOT NULL
@@ -46,9 +100,9 @@ namespace GoodsAuctionWinFormsApp.Control
                     , CONSTRAINT [PK_ITEM] PRIMARY KEY ([ItemId])
                     , FOREIGN KEY([Seller]) REFERENCES [ACCOUNT]([username]));";
 
-                    cmnd.CommandText = table;
-                    cmnd.ExecuteNonQuery();
-                    table = @"CREATE TABLE [LOGOUT] (
+                cmnd.CommandText = table;
+                cmnd.ExecuteNonQuery();
+                table = @"CREATE TABLE [LOGOUT] (
                                 [Id] TEXT NOT NULL
                               , [Date] INTEGER NOT NULL
                               , [Time] INTEGER NOT NULL
@@ -56,14 +110,14 @@ namespace GoodsAuctionWinFormsApp.Control
                               , CONSTRAINT [PK_LOGOUT] PRIMARY KEY ([Id])
                               , FOREIGN KEY([UName]) REFERENCES [ACCOUNT]([username]));";
 
-                    cmnd.CommandText = table;
-                    cmnd.ExecuteNonQuery();
-                    table = @"CREATE TABLE [BIDS] (
+                cmnd.CommandText = table;
+                cmnd.ExecuteNonQuery();
+                table = @"CREATE TABLE [BIDS] (
                                 [ItemNo] TEXT PRIMARY KEY NOT NULL
                               , [BuyerNo] TEXT PRIMARY KEY NOT NULL
                               , [Amount] INTEGER NOT NULL
                               , CONSTRAINT [PK_BIDS] PRIMARY KEY ([ItemNo],[BuyerNo]));";
-                }
+
             }
         }
 
@@ -85,9 +139,9 @@ namespace GoodsAuctionWinFormsApp.Control
 
             sqliteCommand.CommandText = "INSERT INTO ACCOUNT(username" +
                 "                                          , password" +
-                "                                          , type) VALUES ('bidder@email.com'," +
-                "                                           1232590539 + ,'bidder')";
+                "                                          , type)";
             sqliteCommand.ExecuteNonQuery();
+
             sqliteCommand.CommandText = "INSERT INTO ITEM(itemId" +
                 "                                         ,itemName" +
                 "                                         ,itemDescription" +
@@ -95,30 +149,31 @@ namespace GoodsAuctionWinFormsApp.Control
                 "                                         ,currentBid" +
                 "                                         ,timeRemaining" +
                 "                                         ,currentLeader" +
-                "                                         ,Seller) VALUES ('','','','','','','','')";
+                "                                         ,Seller)";
             sqliteCommand.ExecuteNonQuery();
 
             sqliteCommand.CommandText = "INSERT INTO LOGOUT(Id" +
     "                                         ,Date" +
     "                                         ,Time" +
-    "                                         ,UName) VALUES('', '', '', '')";
+    "                                         ,UName)";
             sqliteCommand.ExecuteNonQuery();
 
             sqliteCommand.CommandText = "INSERT INTO BIDS(ItemNo" +
     "                                          , BuyerNo" +
-    "                                          , Amount) VALUES ('','','')";
+    "                                          , Amount)";
             sqliteCommand.ExecuteNonQuery();
         }
-      public static void ReadData(SQLiteConnection conn)
+
+        public static void ReadData(SQLiteConnection conn)
         {
             SQLiteDataReader sqliteReader;
             SQLiteCommand sqliteCommand;
-            sqliteCommand=conn.CreateCommand();
+            sqliteCommand = conn.CreateCommand();
             sqliteCommand.CommandText = "SELECT * FROM ACCOUNT";
-            sqliteReader = sqliteCommand.ExecuteReader();   
-            while(sqliteReader.Read())
+            sqliteReader = sqliteCommand.ExecuteReader();
+            while (sqliteReader.Read())
             {
-                string readerstring = sqliteReader.GetString(0);    
+                string readerstring = sqliteReader.GetString(0);
             }
 
             sqliteCommand.CommandText = "SELECT * FROM ITEM";
@@ -127,7 +182,7 @@ namespace GoodsAuctionWinFormsApp.Control
             {
                 string readerstring = sqliteReader.GetString(0);
             }
-            
+
             sqliteCommand.CommandText = "SELECT * FROM LOGOUT";
             sqliteReader = sqliteCommand.ExecuteReader();
             while (sqliteReader.Read())
@@ -142,67 +197,5 @@ namespace GoodsAuctionWinFormsApp.Control
                 string readerstring = sqliteReader.GetString(0);
             }
         }
-
-        public static Account getUser(string user)
-        {
-            Account apple = null;
-            return apple;
-        }
-        
-//Joy's code
-      //public class DBConnect : Controller
-      //  {
-      //      private Account user;
-      //      private Item itemID;
-      //      private ItemList item;
-      //      private Account username;
-                        
-      //      public DBConnect(Account username)
-      //      {
-      //          this.username = username;
-      //      }
-
-      //      public Account getUser()
-      //      {
-      //          return username;
-      //      }
-      //      public void saveLogout(Account username)
-      //      {
-      //          this.username = username;
-      //      }
-
-      //      public Item getItem(Item itemID)
-      //      {
-      //          //This item ID returns the item
-      //          return null;
-      //      }
-
-      //      public ItemList modifyItem(Item itemID, string newBid, Account username)
-      //      {
-      //          return null;//This is the placebid where it updates the item after bid and 
-      //                      //returns ItemList
-      //          return null;
-      //      }
-
-      //      public ItemList saveItem(Item item)
-      //      {
-      //          //This is the addItem where the data is saved
-      //          return null;
-      //      }
-
-      //      // adding saveLogout(un) from sequence diagram
-      //      public bool saveLogout(string username)
-      //      {
-      //          // not sure if this is right, but I'm trying to check if it saved correctly
-      //          // if true, sends it back to LoginForm for (logged out sucess)
-      //          if (username != null)
-      //          {
-      //              return true;
-      //          }
-      //          else { return false; }
-      //      }
-      //  }
     }
 }
-
-
